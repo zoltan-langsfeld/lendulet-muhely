1a6fcaa9608f4ccc25ecab6c5c8de110d29fd553
Zoltan Lajtavari <lajtavariz@gmail.com>
2020-01-01 19:55:26 +0100

    adds colleagues page

--- src/custom/js/colleagues.js
+++ src/custom/js/colleagues.js
@@ -1,41 +1,49 @@
 const ARANY_ILDIKO = {
-    name: 'Arany Ildikó',
+    name: 'Sándorné Arany Ildikó',
+    title: 'gyógypedagógus, logopédus',
     phoneNr: '+36 70 408 7150',
     email: 'marathon2000@gmail.com'
 };
 
 const BOGAR_JUDIT = {
     name: 'Bogár Judit',
+    title: 'klinikai szakpszichológus',
     phoneNr: '+36 30 273 7739',
     email: 'judibogar@gmail.com'
 };
 
 const FILEP_HAJNALKA = {
     name: 'Filep Hajnalka',
+    title: 'gyermek- és ifjúsági klinikai szakpszichológus, családterapeuta',
     phoneNr: '+36 20 410 1008',
     email: 'neteger@gmail.com'
 };
 
 const PAPP_TIMEA = {
     name: 'Papp Tímea',
+    title: 'gyermek- és ifjúsági klinikai szakpszichológus',
     phoneNr: '+36 20 433 4425',
     email: 'ptmilus@gmail.com'
 };
 
 window.onload = function () {
     $('#arany-ildiko .name').append(ARANY_ILDIKO.name);
+    $('#arany-ildiko .title').append(ARANY_ILDIKO.title);
     $('#arany-ildiko .phone-nr').append(ARANY_ILDIKO.phoneNr);
     $('#arany-ildiko .email').append(ARANY_ILDIKO.email);
 
     $('#bogar-judit .name').append(BOGAR_JUDIT.name);
+    $('#bogar-judit .title').append(BOGAR_JUDIT.title);
     $('#bogar-judit .phone-nr').append(BOGAR_JUDIT.phoneNr);
     $('#bogar-judit .email').append(BOGAR_JUDIT.email);
 
     $('#filep-hajnalka .name').append(FILEP_HAJNALKA.name);
+    $('#filep-hajnalka .title').append(FILEP_HAJNALKA.title);
     $('#filep-hajnalka .phone-nr').append(FILEP_HAJNALKA.phoneNr);
     $('#filep-hajnalka .email').append(FILEP_HAJNALKA.email);
 
     $('#papp-timea .name').append(PAPP_TIMEA.name);
+    $('#papp-timea .title').append(PAPP_TIMEA.title);
     $('#papp-timea .phone-nr').append(PAPP_TIMEA.phoneNr);
     $('#papp-timea .email').append(PAPP_TIMEA.email);
 };
